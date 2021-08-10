4e6f67d757399c2877c881e2c779f186f3e2e1da
rajesh <rajeshsighchufal@outlook.com>
2021-08-10 16:56:01 +0530

    Feat: added search box

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ import Navbar from "./Navbar";
 import Leftbar from "./Leftbar";
 import React,{useState, useEffect} from "react";
 import Rightbar from"./Rightbar";
-import {Row,Col,Modal } from "react-bootstrap";
+import {Row,Col,Modal,Form, InputGroup, FormControl,
+FormGroup,
+ } from "react-bootstrap";
+ import {AiOutlineSearch} from "react-icons/ai";
 import {connect} from "react-redux";
 
 const Home = (props) => {
@@ -20,7 +23,21 @@ const {Activities} = props;
       <div className="d-flex justify-content-between mainpage align-items-center" style={{height:"calc(100vh - 28px)"}}>
 
           <Leftbar />
+
           <Rightbar />
+
+          <div className="searchbox">
+          {Activities? (<Form>
+          <FormGroup>
+            <InputGroup className="mb-0">
+              <InputGroup.Text style={{background: "#2a3133",border: "1px solid rgba(255,255,255,0)", borderRight: "0px"}}
+              className="text-white"><AiOutlineSearch /></InputGroup.Text>
+              <FormControl type="text"  placeholder="Type to search..." style={{background: "#2a3133",
+            border: "1px solid rgba(255,255,255,0)" , borderLeft: "0px"}} />
+            </InputGroup>
+          </FormGroup>
+          </Form>): ("")}
+      </div>
 
       </div>
     </div>
